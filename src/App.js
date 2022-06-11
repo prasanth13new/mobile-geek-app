252d176003e8d5baa01f30d23a0831a091207f63
prasanth13new <prasanth13new@gmail.com>
2022-06-11 10:49:55 +0530

    mobile geek ui

--- src/App.js
+++ src/App.js
@@ -4,22 +4,59 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Phone /> */}
+      <PhoneList />
     </div>
   );
+}
+
+function PhoneList() {
+  const mobiles = [
+    {
+      model: "OnePlus 9 5G",
+      img: "https://m.media-amazon.com/images/I/61fy+u9uqPL._SX679_.jpg",
+      company: "Oneplus"
+    },
+    {
+      model: "Iphone 13 mini",
+      img:
+        "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-mini-blue-select-2021?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1645572315986",
+      company: "Apple"
+    },
+    {
+      model: "Samsung s21 ultra",
+      img: "https://m.media-amazon.com/images/I/81kfA-GtWwL._SY606_.jpg",
+      company: "Samsung"
+    },
+    {
+      model: "Xiomi mi 11",
+      img: "https://m.media-amazon.com/images/I/51K4vNxMAhS._AC_SX522_.jpg",
+      company: "Xiomi"
+    }
+  ];
+
+  return (
+    <div className='phone-list-container'>
+      {mobiles.map((mobile) => (
+      <Phone mobile={mobile} />
+        )) }
+    </div>
+  )
+};
+
+function Phone({mobile}) {
+  // const mobile = {
+  //   company : "Oneplus",
+  //   img: "https://m.media-amazon.com/images/I/61fy+u9uqPL._SX679_.jpg",
+  //   model: "OnePlus 9 5G",
+  // };
+  return (
+    <div className='phone-container'>
+      <img src={mobile.img} alt={mobile.model} className="phone-picture" />
+      <h2 className='phone-name'>{mobile.model}</h2>
+      <p className='phone-company'>{mobile.company}</p>
+    </div>
+  )
 }
 
 export default App;
